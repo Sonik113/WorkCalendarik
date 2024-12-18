72ebb9ca97a3823cb42f0e13453eefdaa2100bf2
Sonik113 <123419503+Sonik113@users.noreply.github.com>
2024-12-18 08:07:12 +0200

    Add files via upload

---
+++ Initializer.cs
@@ -0,0 +1,26 @@
+﻿using WorkCalendarik.Domain.Database.ModelsDb;
+using WorkCalendarik.Domain.Database.Storage;
+using WorkCalendarik.Domain.Interfaces;
+using WorkCalendarik.Service.Interfaces;
+using WorkCalendarik.Service.Realizations;
+
+namespace WorkCalendarik;
+
+public static class Initializer
+{
+    public static void InitializeRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IBaseStorage<UserDb>, UserStorage>();
+        services.AddScoped<IBaseStorage<BronCalendarDb>, BronCalendarStorage>();
+    }
+    
+    public static void InitializeServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAccountService, AccountService>();
+        services.AddScoped<IBronCalendarService, BronCalendarService>();
+        
+        services.AddControllersWithViews()
+            .AddDataAnnotationsLocalization()
+            .AddViewLocalization();
+    }
+}
